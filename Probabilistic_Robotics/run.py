143d922971aa3000800821d19afda3b2afb16910
johkakka <johkakka.rits@gmail.com>
2020-05-24 21:37:24 +0900

    let robots have sensor

--- Probabilistic_Robotics/run.py
+++ Probabilistic_Robotics/run.py
@@ -1,11 +1,10 @@
 import numpy as np
 import math
 
-from Robot import Robot
+from Robot import *
 from World import World
 from Agent import Agent
-from Map import Map
-from Map import Landmark
+from Map import *
 
 world = World()
 
@@ -18,8 +17,8 @@ world.append(m)
 agent1 = Agent(0.2, 0.0)
 agent2 = Agent(0.2, 10.0/180*math.pi)
 
-robot1 = Robot(np.array([2, 3, math.pi/5*6]).T, agent=agent1)
-robot2 = Robot(np.array([1, 0, math.pi/5*6]).T, agent=agent2, color='red')
+robot1 = Robot(np.array([2, 3, math.pi/5*6]).T, sensor=Camera(m), agent=agent1)
+robot2 = Robot(np.array([1, 0, math.pi/5*6]).T, agent=agent2, sensor=Camera(m), color='red')
 robot3 = Robot(np.array([0, 0, 0]).T, color='blue')
 
 world.append(robot1)
