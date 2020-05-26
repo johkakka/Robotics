2528ca1001fc23e3b6e3df442216c0369dbc1548
johkakka <johkakka.rits@gmail.com>
2020-05-27 00:10:58 +0900

    パーティクルの姿勢の尤度を評価

--- Probabilistic_Robotics/EstimationModule/MCL.py
+++ Probabilistic_Robotics/EstimationModule/MCL.py
@@ -16,6 +16,7 @@ class EstimationAgent(Agent):
     def decision(self, observation=None):
         self.estimator.update(self.prev_nu, self.prev_omega, self.interval)
         self.prev_nu, self.prev_omega = self.nu, self.omega
+        self.estimator.observation_update(observation)
         return self.nu, self.omega
 
     def draw(self, ax, elems):
@@ -23,8 +24,23 @@ class EstimationAgent(Agent):
 
 
 class Particle:
-    def __init__(self, pose):
+    def __init__(self, pose, weight):
         self.pose = pose
+        self.weight = weight
+
+    def observation_update(self, observation, envmap, distance_dev_rate, direction_dev):
+        for d in observation:
+            obs_pos = d[0]
+            obs_id = d[1]
+
+            ###パーティクルの位置と地図からランドマークの距離と方角を算出###
+            pos_on_map = envmap.landmarks[obs_id].pos
+            particle_suggest_pos = IdealCamera.observation_function(self.pose, pos_on_map)
+
+            ###尤度の計算###
+            distance_dev = distance_dev_rate * particle_suggest_pos[0]
+            cov = np.diag(np.array([distance_dev ** 2, direction_dev ** 2]))
+            self.weight *= multivariate_normal(mean=particle_suggest_pos, cov=cov).pdf(obs_pos)
 
     def update(self, nu, omega, time, noise_rate_pdf):
         ns = noise_rate_pdf.rvs()
@@ -34,20 +50,30 @@ class Particle:
 
 
 class MCL:
-    def __init__(self, pose, num, motion_noise_stds={"nn":0.19, "no":0.001, "on":0.13, "oo":0.2}):
-        self.particles = [Particle(pose) for i in range(num)]
+    def __init__(self, envmap, init_pose, num, motion_noise_stds=None,
+                 distance_dev_rate=0.14, direction_dev=0.05):
+        if motion_noise_stds is None:
+            motion_noise_stds = {"nn": 0.19, "no": 0.001, "on": 0.13, "oo": 0.2}
+        self.particles = [Particle(init_pose, 1.0 / num) for i in range(num)]
+        self.map = envmap
+        self.distance_dev_rate = distance_dev_rate
+        self.direction_dev = direction_dev
 
         v = motion_noise_stds
-        c = np.diag([v["nn"]**2, v["no"]**2, v["on"]**2, v["oo"]**2])
+        c = np.diag([v["nn"] ** 2, v["no"] ** 2, v["on"] ** 2, v["oo"] ** 2])
         self.motion_noise_rate_pdf = multivariate_normal(cov=c)
 
-    def draw(self, ax, elems):
+    def update(self, nu, omega, time):
+        for p in self.particles: p.update(nu, omega, time, self.motion_noise_rate_pdf)
+
+    def observation_update(self, observation):
+        for p in self.particles: p.observation_update(observation, self.map, self.distance_dev_rate,
+                                                      self.direction_dev)
+
+    def draw(self, ax, elems):  # 次のように変更
         xs = [p.pose[0] for p in self.particles]
         ys = [p.pose[1] for p in self.particles]
-        vxs = [math.cos(p.pose[2]) for p in self.particles]
-        vys = [math.sin(p.pose[2]) for p in self.particles]
-        elems.append(ax.quiver(xs, ys, vxs, vys, color="blue", alpha=0.3))
-
-    def update(self, nu, omega, time):
-        for p in self.particles:
-            p.update(nu, omega, time, self.motion_noise_rate_pdf)
+        vxs = [math.cos(p.pose[2]) * p.weight * len(self.particles) for p in self.particles]
+        vys = [math.sin(p.pose[2]) * p.weight * len(self.particles) for p in self.particles]
+        elems.append(ax.quiver(xs, ys, vxs, vys,
+                               angles='xy', scale_units='xy', scale=1.5, color="blue", alpha=0.3))
--- Probabilistic_Robotics/run_MCL.py
+++ Probabilistic_Robotics/run_MCL.py
@@ -9,10 +9,14 @@ def main():
     time_interval = 0.1  ###draw_mcl7###
     world = World(30, time_interval, debug=False)
 
+    m = Map()
+    for ln in [(-4, 2), (2, -3), (3, 3)]: m.append_landmark(Landmark(*ln))
+    world.append(m)
+
     initial_pose = np.array([0, 0, 0]).T
-    estimator = MCL(initial_pose, 1000)
+    estimator = MCL(m, initial_pose, 100)
     circling = EstimationAgent(time_interval, 0.2, 10.0 / 180 * math.pi, estimator)
-    r = Robot(initial_pose, sensor=None, agent=circling, color="red")
+    r = Robot(initial_pose, sensor=Camera(m), agent=circling, color="red")
     world.append(r)
 
     world.draw()
